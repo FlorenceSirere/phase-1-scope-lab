049a0ee412d19879c89f162491617b4790f3cf17
Moringa School <sirerekatimwa@gmail.com>
2022-09-27 08:28:15 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+window.customerName = "bob"
+window.bestcustomer
+const least_favourite_costomer =""
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestcustomer = 'not bob'; 
+}
+
+function overwriteBestcustomer(){
+    bestcustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    least_favorite_customer = "kev"
+}
